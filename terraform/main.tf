bc1ad8715980fd28557880df884a2e7757ec4b1b
Rafe <stefanorafe@gmail.com>
2025-09-27 01:42:19 +0200

    modifies

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,29 +2,23 @@ provider "azurerm" {
   features {}
 }
 
-# ------------------------
-# Network stack
-# ------------------------
 module "network" {
   source   = "./modules/network"
   rg_name  = var.rg_name
   location = var.location
 }
 
-# ------------------------
-# Database stack
-# ------------------------
 module "database" {
   source        = "./modules/database"
   rg_name       = var.rg_name
   location      = var.location
   vnet_id       = module.network.vnet_id
   db_subnet_id  = module.network.db_subnet_id
+
+  db_admin_username = var.db_admin_username
+  db_admin_password = var.db_admin_password
 }
 
-# ------------------------
-# Compute stack (Grafana VMSS + LB + DNS)
-# ------------------------
 module "compute" {
   source            = "./modules/compute"
   rg_name           = var.rg_name
@@ -32,11 +26,12 @@ module "compute" {
   app_subnet_id     = module.network.app_subnet_id
   lb_public_ip_name = "grafana-lb-ip"
   db_connection     = module.database.db_connection
+
+  dns_rg_name   = var.dns_rg_name
+  dns_zone_name = var.dns_zone_name
+  ssh_public_key = var.ssh_public_key
 }
 
-# ------------------------
-# Observability stack (Monitoring + Logs + Alerts)
-# ------------------------
 module "observability" {
   source          = "./modules/observability"
   rg_name         = var.rg_name
